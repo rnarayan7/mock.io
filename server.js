4096cd59922891ccba87c205d375dcd13c11a853
Roshan Narayan <rnarayan7@users.noreply.github.com>
2018-12-16 05:57:06 -0500

    Change to use static webpages

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const path = require(`path`);
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
-});
+app.use(express.static('src'))
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
