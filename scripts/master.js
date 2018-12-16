d79c3ba31817772f505978331a8a702c14d5bebd
Roshan Narayan <rnarayan7@users.noreply.github.com>
2018-12-15 23:15:15 -0500

    Accessibility and content changes for index.html

--- scripts/master.js
+++
@@ -1,5 +0,0 @@
-function highlightPage(page){
-    var selector_id = "#" + page + "-link";
-    $("nav").children(selector_id).css({ "border-style": "solid",
-                                         "border-width": "1.5px"});
-}
