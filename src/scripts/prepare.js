e7db9abdae4ba4a6565e758ced106b5583d83772
Roshan Narayan <rnarayan7@users.noreply.github.com>
2018-12-16 20:04:42 -0600

    Add full accordion with working functionality

--- src/scripts/prepare.js
+++ src/scripts/prepare.js
@@ -1,19 +1,28 @@
-function initAccordion(accordionElem) {
+// Function to create accordion
+function initAccordion(accordionElem){
+    
+    // Event handler for when button for accordion is selected
     function handlePanelClick(event){
         showPanel(event.currentTarget);
     }
-    function showPanel(panel){
-        var expandedPanel = accordionElem.querySelectorAll(".active")
-        if(expandedPanel){
-            expandedPanel.classList.remove("active");
-        }
-        panel.classList.add(active);
-    }
-    var allPanelElemts = accordionElem.querySelectorAll(".panel");
-    for (var i = 0; len = allPanelElems.length; i < len; i++){
-        allPanelElems[i].addEventListener("click", handlePanelClick)
-    }
-    showPanel(allPanelElems[0]);
-}
 
-initAccordion(document.getElementById("accordion"))
+    // Shows or hides panel based on selection
+    function showPanel(panel){
+        var isAlreadyActive = panel.querySelector(".accordion-body").classList.contains("active")
+
+        var expandedPanel = $(".active");
+        if (expandedPanel){
+            expandedPanel.removeClass("active");
+        }
+        if(isAlreadyActive != true) {
+            panel.querySelector(".accordion-body").classList.add("active");
+        }
+    }
+
+    // Assigns click event to all panel elements
+    var allPanelElems = $(".accordion-panel");
+    console.log(allPanelElems)
+    allPanelElems.on("click",handlePanelClick)
+}
+// Call initialization for accordion
+initAccordion($("#accordion"));
